df0c2f62560db38861bc38a0f5d817f9a8ec4188
sigopensando <carlosalvarezing19@gmail.com>
2026-08-23 02:25:37 -0600

    feat: checkpoint 4 - Feature extraction, with BoW (Bag of Words)

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from pydoc import doc
-
 import spacy
 import pandas as pd
 from nltk.stem import SnowballStemmer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
 
 def tokenize(text):
@@ -64,6 +68,31 @@ def lemmatize(doc):
     return normalized_tokens
 
 
+def plot_words_3d(ax, matrix, vocabulary, title, points_color):
+    words_matrix = matrix.T
+
+    pca = PCA(n_components=3)
+    coords = pca.fit_transform(words_matrix.toarray())
+
+    x = coords[:, 0]
+    y = coords[:, 1]
+    z = coords[:, 2]
+
+    ax.scatter(x, y, z, c=points_color, s=80, edgecolors='k', alpha=0.8, depthshade=True)
+
+    for i, word in enumerate(vocabulary):
+        ax.text(x[i], y[i], z[i] + 0.1, word, fontsize=9)
+
+    ax.set_title(title)
+    ax.set_xlabel("Comp. Principal 1")
+    ax.set_ylabel("Comp. Principal 2")
+    ax.set_zlabel("Comp. Principal 3")
+
+    ax.plot([0,0], [0,0], [z.min(), z.max()], c='gray', linestyle='--', linewidth=0.5)
+    ax.plot([x.min(), x.max()], [0,0], [0,0], c='gray', ls='--', lw=0.5, alpha=0.5)
+    ax.plot([0,0], [y.min(), y.max()], [0,0], c='gray', ls='--', lw=0.5, alpha=0.5)
+
+
 def main():
     with open("books/la_biblioteca_de_babel.txt", "r", encoding="utf-8") as f:
         text = f.read()
@@ -96,6 +125,40 @@ def main():
 
     print("\n--- Visualización completa de los primeros 10 tokens ---")
     print(df.head(10).to_string(index=False))
+
+    corpus_lemmatized = []
+
+    for sentence in doc.sents:
+        sentence_lemmas = [
+            token.lemma_.lower()
+            for token in sentence
+            if not token.is_punct and not token.is_space and not token.is_stop
+        ]
+        if sentence_lemmas:
+            corpus_lemmatized.append(" ".join(sentence_lemmas))
+
+    print(f"Total de oraciones procesadas: {len(corpus_lemmatized)}")
+
+    fig = plt.figure(figsize=(18, 8))
+
+    ax1 = fig.add_subplot(121, projection='3d')
+
+    bow_vectorizer = CountVectorizer()
+    X_bow = bow_vectorizer.fit_transform(corpus_lemmatized)
+    bow_vocabulary = bow_vectorizer.get_feature_names_out()
+
+    plot_words_3d(ax1, X_bow, bow_vocabulary, "Espacio BoW 3D (Conteos)", "orange")
+
+    ax2 = fig.add_subplot(122, projection='3d')
+
+    tfidef_vectorizer = TfidfVectorizer()
+    X_tfidf = tfidef_vectorizer.fit_transform(corpus_lemmatized)
+    tfidf_vocabulary = tfidef_vectorizer.get_feature_names_out()
+
+    plot_words_3d(ax2, X_tfidf, tfidf_vocabulary, "Espacio TF-IDF 3D (Importancia)", "teal")
+
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
